ba382801a94f4b83d6b565aaacd7efdcda8c1749
LorgeN <eirik@tanberg.org>
2021-10-08 15:30:37 +0200

    Final touches

--- Lab2/cache_sim.c
+++ Lab2/cache_sim.c
@@ -6,6 +6,8 @@
 #include <stdbool.h>
 #include <math.h>
 
+// Macro for computing how many bits are required to store unsigned
+// values up the the given value
 #define BIT_WIDTH(value) log2(value)
 
 typedef enum
@@ -46,36 +48,62 @@ typedef struct
     uint64_t inserted_at; // To implement FIFO for FA
 } cache_line_t;
 
+/**
+ * A struct for a simulated cache. Does not specify type or 
+ * mapping. Simply intended to keep track of different values.
+ */
 typedef struct
 {
     uint32_t blocks;
+    // We could potentially store the bit masks here since they
+    // wont change during runtime
     uint32_t bits_offset;
     uint32_t bits_index;
     uint32_t bits_tag;
+    cache_stat_t statistics;
     cache_line_t lines[];
 } cache_t;
 
+/**
+ * Utility struct for simplifying the transition between unified
+ * and split cache.
+ */
 typedef struct
 {
     cache_t *instructions;
     cache_t *data;
 } cache_total_t;
 
+/**
+ * Gets the tag for the address of the given memory access in the
+ * given cache
+ */
 uint32_t get_tag(cache_t cache, mem_access_t access)
 {
+    // We compute the bit mask here, and then use a bitwise AND.
+    // Sets every bit that we don't care about to 0. We could shift
+    // also shift this to the right so we only keep data in the least
+    // significant bits but for our usecase this is unnecessary.
     return access.address & ~(0xFFFFFFFF >> cache.bits_tag);
 }
 
+/**
+ * Allocates a new cache and initializes the values
+ */
 cache_t *make_cache(uint32_t size, uint32_t block_size, cache_map_t map)
 {
     uint32_t blocks = size / block_size;
+    // Making sure we don't end up in a situation where we have 0 blocks,
+    // which will give weird results later.
     if (blocks == 0)
     {
         printf("Invalid cache size %d! Needs to be larger than %d", size, block_size);
         exit(1);
     }
 
-    size_t mem_size = sizeof(cache_t) + sizeof(cache_line_t) * blocks;
+    // Compute the amount of memory we need to allocate, allocate it
+    // and then make sure that the memory doesn't contain any old values
+    size_t mem_size = sizeof(cache_t) + (sizeof(cache_line_t) * blocks);
     cache_t *cache = malloc(mem_size);
     memset(cache, 0, mem_size);
 
@@ -89,33 +117,48 @@ cache_t *make_cache(uint32_t size, uint32_t block_size, cache_map_t map)
 
 cache_total_t *make_total_cache(uint32_t size, uint32_t block_size, cache_map_t map, cache_org_t org)
 {
+    // We don't need to memset this because we initialize all values later
     cache_total_t *cache = malloc(sizeof(cache_total_t));
+
+    printf("Cache Organization\n");
+    printf(" -------------------- \n");
 
     if (org == sc)
     {
-        size >>= 1; // Divide by 2
+        size >>= 1; // Divide by 2 since the caches should be of equal size
 
         cache->data = make_cache(size, block_size, map);
         cache->instructions = make_cache(size, block_size, map);
     }
     else
     {
+        // Unified cache just means we set the pointers to the same location
         cache_t *unified = make_cache(size, block_size, map);
         cache->data = unified;
         cache->instructions = unified;
     }
 
+    printf("Cache size: %d\n", size);
+    printf("Mapping: %s\n", map == dm ? "Direct Mapped" : "Fully Associative");
+    printf("Organization: %s\n", org == sc ? "Split Cache" : "Unified Cache");
+    printf("Offset: %d\n", cache->data->bits_offset);
+    printf("Index: %d\n", cache->data->bits_index);
+    printf("Tag: %d\n", cache->data->bits_tag);
+
     return cache;
 }
 
+/**
+ * Simulate memory access with fully associative cache
+ */
 void access_mem_fa(cache_t *cache, cache_stat_t *statistics, mem_access_t access)
 {
     uint32_t tag = get_tag(*cache, access);
 
     statistics->accesses++;
+    cache->statistics.accesses++;
 
-    cache_line_t line;
-    uint32_t index;
+    uint32_t index = 0;
 
     for (uint32_t i = 0; i < cache->blocks; i++)
     {
@@ -123,14 +166,12 @@ void access_mem_fa(cache_t *cache, cache_stat_t *statistics, mem_access_t access
         if (!curr.valid)
         {
             index = i;
-            line = curr;
             continue;
         }
 
-        if (!&line || line.inserted_at > curr.inserted_at)
+        if (cache->lines[index].inserted_at > curr.inserted_at)
         {
             index = i;
-            line = curr;
         }
 
         if (curr.tag != tag)
@@ -138,11 +179,15 @@ void access_mem_fa(cache_t *cache, cache_stat_t *statistics, mem_access_t access
             continue;
         }
 
+        cache->statistics.hits++;
         statistics->hits++;
         return;
     }
 
-    // Only keep track of when inserted since we are implementing a FIFO queue
+    cache_line_t line = cache->lines[index];
+
+    // Only keep track of when inserted since we are implementing a FIFO queue.
+    // Change this to update with every access to implement LRU cache.
     line.inserted_at = statistics->accesses;
     line.valid = true;
     line.tag = tag;
@@ -150,16 +195,28 @@ void access_mem_fa(cache_t *cache, cache_stat_t *statistics, mem_access_t access
     cache->lines[index] = line;
 }
 
+/**
+ * Gets the index of the given memory access in the given cache when
+ * using direct mapping
+ */
 uint32_t get_index(cache_t cache, mem_access_t access)
 {
+    // Make bit mask to grab the index bits
     uint32_t mask = ~(0xFFFFFFFF << cache.bits_index) << cache.bits_offset;
     return (access.address & mask) >> cache.bits_offset;
 }
 
+/**
+ * Simulate memory access with directly mapped cache
+ */
 void access_mem_dm(cache_t *cache, cache_stat_t *statistics, mem_access_t access)
 {
     uint32_t tag = get_tag(*cache, access);
     uint32_t index = get_index(*cache, access);
+
+    // This shouldn't ever happen, but we check it here to make sure we
+    // don't step outside the allocated memory which could cause unforeseen
+    // issues
     if (index >= cache->blocks)
     {
         printf("Illegal access! Index: %d, max: %d\n", index, cache->blocks);
@@ -168,12 +225,14 @@ void access_mem_dm(cache_t *cache, cache_stat_t *statistics, mem_access_t access
     }
 
     statistics->accesses++;
+    cache->statistics.accesses++;
 
     cache_line_t line = cache->lines[index];
 
     if (line.valid && line.tag == tag)
     {
         statistics->hits++;
+        cache->statistics.hits++;
         return;
     }
 
@@ -311,7 +370,7 @@ void main(int argc, char **argv)
         //If no transactions left, break out of loop
         if (access.address == 0)
             break;
-        printf("%d %x\n", access.accesstype, access.address);
+        //printf("%d %x\n", access.accesstype, access.address);
 
         /* Do a cache access */
 
@@ -336,6 +395,18 @@ void main(int argc, char **argv)
     printf("Hits:     %ld\n", cache_statistics.hits);
     printf("Hit Rate: %.4f\n", (double)cache_statistics.hits / cache_statistics.accesses);
     // You can extend the memory statistic printing if you like!
+
+    if (cache_org == sc)
+    {
+        printf("\n");
+        printf("DCache Accesses: %ld\n", cache->data->statistics.accesses);
+        printf("DCache Hits:     %ld\n", cache->data->statistics.hits);
+        printf("DCache Hit Rate: %.4f\n", (double)cache->data->statistics.hits / cache->data->statistics.accesses);
+        printf("\n");
+        printf("ICache Accesses: %ld\n", cache->instructions->statistics.accesses);
+        printf("ICache Hits:     %ld\n", cache->instructions->statistics.hits);
+        printf("ICache Hit Rate: %.4f\n", (double)cache->instructions->statistics.hits / cache->instructions->statistics.accesses);
+    }
 
     /* Close the trace file */
     fclose(ptr_file);
